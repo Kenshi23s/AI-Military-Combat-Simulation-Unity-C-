5d18aff95325becf3551ff6b497220c761de0bcc
Kenshi23s <facucolombo@gmail.com>
2023-07-08 14:01:23 -0300

    Update Infantry.cs

--- Assets/Scripts/Entity/Human/Infantry.cs
+++ Assets/Scripts/Entity/Human/Infantry.cs
@@ -18,14 +18,15 @@ public abstract class Infantry : Entity
         FireAtWill
     }
    
-    AI_Movement _ai_Move;
+    AI_Movement _infantry_AI;
     FOVAgent _fov;
     Fireteam myFireteam;
     public EventFSM<INFANTRY_STATES> infantry_FSM;
-        
+
+    public Entity actualTarget;
     protected override void EntityAwake()
     {
-        _ai_Move = GetComponent<AI_Movement>();
+        _infantry_AI = GetComponent<AI_Movement>();
         _fov = GetComponent<FOVAgent>();
     }
 
@@ -40,39 +41,80 @@ public abstract class Infantry : Entity
 
         state.OnEnter += (x) =>
         {
-            _ai_Move.SetDestination(myFireteam.newPos);
+            _infantry_AI.SetDestination(myFireteam.newPos);
             StartCoroutine(LookForTargets());
         };
-
-
 
         state.OnExit += (x) => 
         { 
             StopCoroutine(LookForTargets());
-            _ai_Move.CancelMovement();
+            _infantry_AI.CancelMovement();
         };
 
         return state;
     }
 
+    #region Metodos Utiles
     IEnumerator LookForTargets()
     {
         while (true)
         {
-            var z = gridEntity.GetEntitiesInRange(_fov.viewRadius)
-           .Where(x => x != this)
-           .OfType<Entity>()
-           .Where(x => x.myTeam!=myTeam)
+            var z = GetEntitiesAround()                               
+           .Where(x => x.myTeam != myTeam)
            .Where(x => _fov.IN_FOV(x.transform.position));
 
             if (z.Any()) infantry_FSM.SendInput(INFANTRY_STATES.FireAtWill);
 
-            for (int i = 0; i < 30; i++)           
-                yield return null;           
+            for (int i = 0; i < 30; i++)
+                yield return null;
         }
     }
 
 
+    IEnumerable<Entity> GetEntitiesAround()
+    {
+        var z = gridEntity.GetEntitiesInRange(_fov.viewRadius)
+         .Where(x => x != this)
+         .OfType<Entity>()
+         .Where(x => x.GetType() != typeof(Civilian));
+
+
+
+        return z;
+    }
+
+    IEnumerator SetTarget()
+    {
+        while (true)
+        {
+           actualTarget = GetEntitiesAround().Minimum(GetWeakestAndNearest);
+            if (actualTarget)
+            {
+
+            }
+        }
+    }
+    #endregion
+
+    float GetWeakestAndNearest(Entity entity)
+    {
+        float result=0;
+        result += Vector3.Distance(transform.position, entity.transform.position);
+        result += entity.health.life;
+        return result;
+    }
+
+    State<INFANTRY_STATES> FireAtWill()
+    {
+        State<INFANTRY_STATES> state = new State<INFANTRY_STATES>("FireAtWill");
+
+        state.OnEnter += (x) => 
+        { 
+
+        };
+
+        return state;
+    }
 
     public void MoveTowardsTransition()
     {
@@ -82,15 +124,5 @@ public abstract class Infantry : Entity
     public void FollowLeaderTransition()
     {
         infantry_FSM.SendInput(INFANTRY_STATES.FollowLeader);
-    }
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
     }
 }
