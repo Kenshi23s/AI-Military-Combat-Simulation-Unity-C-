04ebb4d65293ea8e3cb035ffad96f41740a1ecd2
Kenshi23s <facucolombo@gmail.com>
2023-07-12 17:49:32 -0300

    Fireteam Avances

--- Assets/Scripts/Teams/Fireteam/Fireteam.cs
+++ Assets/Scripts/Teams/Fireteam/Fireteam.cs
@@ -1,5 +1,4 @@
 using FacundoColomboMethods;
-using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -12,13 +11,15 @@ public class Fireteam : MonoBehaviour
     public ReadOnlyCollection<Infantry> fireteamMembers;
     public Infantry Leader { get; private set; }
 
-    public Vector3 newPos;
+    public Team myTeam { get; private set; }
+
+   
+    public float MinimumDistanceFromLeader { get; private set; }
     #region MemberManagment
     public void AddMember(Infantry infantry)
     {
         if (!_fireteamMembers.Contains(infantry))        
-            _fireteamMembers.Add(infantry);          
-        
+            _fireteamMembers.Add(infantry);                  
     }
 
     public void AddMember(IEnumerable<Infantry> infantry)
@@ -49,22 +50,103 @@ public class Fireteam : MonoBehaviour
         }
     }
 
+    public Vector3 LookForNearestZone()
+    {
+        //obtengo mis zonas en disputa
+        var disputedMine = CapturePointManager.instance.CapturePoints.Where(x => x.Key == myTeam)
+            .SelectMany(x => x.Value)
+            .Where(x => x.CurrentCaptureState == CapturePoint.ZoneStates.Disputed)
+            .Select(x => x);
 
+        //y las zonas neut
+        var enemyZones = CapturePointManager.instance.CapturePoints
+            .Where(x => x.Key != myTeam)
+            .SelectMany(x => x.Value)
+            .Select(x => x);
+        //las uno
+        var concat = disputedMine.Concat(enemyZones).Distinct();
+        //y obtengo las mas cercanas, pongo el distinct por las dudas de q haya alguno repetido
+        //Distinct: saca los elementos que sean iguales o esten "clonados"
 
-    public void SendOrders()
+        return concat.Distinct().Minimum(x => Vector3.Distance(x.transform.position,Leader.transform.position)).transform.position;
+    
+    }
+
+    void SendPatrolOrders(Vector3 NewDestination)
     {
         //lookup divide una coleccion en base a un predicado, no solo se puede dividir en booleanas
         //tambien se puede dividir en strings o enums
+
+        //deberia poner a donde debe ir el lider
+
         var split = _fireteamMembers.ToLookup(x => x == Leader);
 
-        split[true].First().MoveTowardsTransition();
+        split[true].First().MoveTowardsTransition(NewDestination);
 
         foreach (var members in split[false]) members.FollowLeaderTransition();
     }
 
     public IEnumerable<Mechanic> GetMechanics() => _fireteamMembers.OfType<Mechanic>();
 
-    public IEnumerable<Medic> GetMedics() => _fireteamMembers.OfType<Medic>();
-    
+    public IEnumerable<Medic> GetMedics() => _fireteamMembers.Select(x => x as GridEntity).OfType<Medic>();
 
+    #region UsefulQuestions
+    //pregunta si algun aliado de la escuadra tiene enemigos cerca
+    public bool AlliesWithEnemiesNearby(Entity whoAsks,out Entity neartestInDanger)
+    {
+        neartestInDanger = null;
+        var col = fireteamMembers.Where(x => x != whoAsks).Where(x => x.GetEntitiesAround().Any(x => x.health.isAlive && x.MyTeam != myTeam));
+        //si tiene algun aliado de la escuadra que no sea el con enemigo cerca que tengan vida
+        if (col.Any())
+        {
+            //le dice que es verdadero y le devuelve el mas cercano en peligro
+            neartestInDanger = col.Minimum(x => Vector3.Distance(x.transform.position, whoAsks.transform.position));
+            return true;
+        }
+        return false;
+    }
+
+    public bool FireteamInCombat()
+    {
+        foreach (var item in fireteamMembers)
+        {
+            if (item.inCombat)
+                return true;
+        }
+
+        return false;
+    }
+
+    public bool IsNearLeader(Infantry member)
+    {
+        return Vector3.Distance(Leader.transform.position,member.transform.position) < MinimumDistanceFromLeader;
+    }
+    #endregion
+
+    public void RequestSupport(Vector3 enemyPosition)
+    {
+        //si hay un avion cercano para bombardeo
+        var planes = TeamsManager.instance.GetTeamPlanes(myTeam).Where(x => x.actualState == PlaneStates.FLY_AROUND);
+        if (planes.Any())
+        {       //lo pido
+            planes.PickRandom().CallAirStrike(enemyPosition);
+            return;
+        }
+                  
+        //sino, pido apoyo a alguna escuadra que no este en combate
+        var nearestFireteam = TeamsManager.instance
+            .GetAllyFireteams(myTeam)
+            .Where(x => x.FireteamInCombat())
+            .Minimum(x => Vector3.Distance(x.Leader.transform.position,transform.position));
+        if (nearestFireteam != null)       
+            nearestFireteam.HelpNearFireteam(enemyPosition);
+        
+    }
+
+    public void HelpNearFireteam(Vector3 EnemyPos)
+    {
+        Leader.MoveTowardsTransition(EnemyPos);
+    }
+
+   
 }
